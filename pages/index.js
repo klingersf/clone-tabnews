5a10ba50ea90d2e8775705d095f674983a010127
klingersf <klinger176@gmail.com>
2023-11-22 00:45:29 -0300

    feat: first page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home () {
+    return (
+        <>
+            <h1>Flávia, meu amor por você é do tamanho do universo!</h1>
+            <h2>Johan e Ethan, vocês fazem o universo expandir cada vez mais!</h2>
+        </>
+    )
+}
+
+export default Home;
