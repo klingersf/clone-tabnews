9c2ad9f2d1160f645da4ed006a7fc7a9e382c619
Klinger Ferreira <60621086+klingersf@users.noreply.github.com>
2023-12-06 18:25:07 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,21 +6,29 @@ async function status(request, response) {
   const databaseVersionResult = await database.query(`SHOW server_version;`);
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const dataOfDbConnections = await database.query(`
-  SELECT max_conn, used, res_for_super, max_conn - used - res_for_super AS res_for_normal
-  FROM 
-    (SELECT count(*) AS used FROM pg_stat_activity) AS t1,  
-    (SELECT setting::int AS res_for_super FROM pg_settings WHERE name = 'superuser_reserved_connections') AS t2,  
-    (SELECT setting::int AS max_conn FROM pg_settings WHERE name = 'max_connections') AS t3
-`);
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue = Number(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue = Number(
+    databaseOpenedConnectionsResult.rows[0].count,
+  );
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
-        max_connections: dataOfDbConnections.rows[0].max_conn,
-        opened_connections: Number(dataOfDbConnections.rows[0].used),
         version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
       },
     },
   });
--- tests/integration/api/status/get.test.js
+++ tests/integration/api/status/get.test.js
@@ -1,24 +1,29 @@
-test("GET to /api/v1/status should return 200", async () => {
+test("Retrieving current system status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
-
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
-
+  const database = responseBody.dependencies.database;
   const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(response.status).toBe(200);
+  expect(database).toBeDefined();
+
+  expect(responseBody.updated_at).toBeDefined();
   expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
-  expect(responseBody.dependencies.database.max_connections).toBeDefined();
-  expect(typeof responseBody.dependencies.database.max_connections).toBe(
-    "number",
-  );
+  expect(Object.keys(database)).toEqual([
+    "version",
+    "max_connections",
+    "opened_connections",
+  ]);
 
-  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
-  expect(typeof responseBody.dependencies.database.opened_connections).toBe(
-    "number",
-  );
+  expect(typeof database.max_connections).toBe("number");
+  expect(database.max_connections).toBeGreaterThanOrEqual(1);
+  expect(database.max_connections).toBe(100);
 
-  expect(responseBody.dependencies.database.version).toBeDefined();
-  expect(typeof responseBody.dependencies.database.version).toBe("string");
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(typeof database.opened_connections).toBe("number");
+  expect(database.opened_connections).toBeGreaterThanOrEqual(1);
+  expect(database.opened_connections).toBe(1);
+
+  expect(typeof database.version).toBe("string");
+  expect(database.version).toEqual("16.0");
 });
