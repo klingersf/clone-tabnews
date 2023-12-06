082fe02254054ea107666d27a7d7b422cf096c49
Klinger Ferreira <60621086+klingersf@users.noreply.github.com>
2023-12-06 05:06:19 +0000

    add status information and types to Jest

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
-async function index(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query(`SHOW server_version`);
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const dataOfDbConnections = await database.query(`
+  SELECT max_conn, used, res_for_super, max_conn - used - res_for_super AS res_for_normal
+  FROM 
+    (SELECT count(*) AS used FROM pg_stat_activity) AS t1,  
+    (SELECT setting::int AS res_for_super FROM pg_settings WHERE name = 'superuser_reserved_connections') AS t2,  
+    (SELECT setting::int AS max_conn FROM pg_settings WHERE name = 'max_connections') AS t3
+`);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: dataOfDbConnections.rows[0].max_conn,
+        opened_connections: Number(dataOfDbConnections.rows[0].used),
+        version: databaseVersionValue,
+      },
+    },
+  });
 }
 
-export default index;
+export default status;
--- tests/integration/api/status/get.test.js
+++ tests/integration/api/status/get.test.js
@@ -1,4 +1,25 @@
-test("GET do api/v1/status should return 200", async () => {
+test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(typeof responseBody.dependencies.database.max_connections).toBe(
+    "number",
+  );
+
+  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
+  expect(typeof responseBody.dependencies.database.opened_connections).toBe(
+    "number",
+  );
+
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(typeof responseBody.dependencies.database.version).toBe("string");
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
 });
