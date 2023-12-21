97d7b960fa460b3a5d8e756dcf019f3ac5190453
Klinger Ferreira <60621086+klingersf@users.noreply.github.com>
2023-12-21 14:34:54 -0300

    Create dbase.js

---
+++ infra/dbase.js
@@ -0,0 +1,25 @@
+import { Client } from "pg";
+
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+
+async function query(queryObject) {
+  try {
+    const result = queryObject;
+    return result;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.end();
+  }
+}
+
+export default {
+  query: query,
+};
