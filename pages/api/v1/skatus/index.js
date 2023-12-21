e0e88f8facba24296cc5a6cdfcdf0043b24660dc
Klinger Ferreira <60621086+klingersf@users.noreply.github.com>
2023-12-21 14:59:26 -0300

    Update index.js

--- pages/api/v1/skatus/index.js
+++ pages/api/v1/skatus/index.js
@@ -1,26 +1,11 @@
-import database from "infra/database.js";
+import health from "models/health.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult = await database.query(`SHOW server_version;`);
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
-
-  const databaseMaxConnectionsResult = await database.query(
-    "SHOW max_connections;",
-  );
-  const databaseMaxConnectionsValue = Number(
-    databaseMaxConnectionsResult.rows[0].max_connections,
-  );
-
-  const databaseName = process.env.POSTGRES_DB;
-  const databaseOpenedConnectionsResult = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
-  const databaseOpenedConnectionsValue = Number(
-    databaseOpenedConnectionsResult.rows[0].count,
-  );
+  const databaseVersionValue = await health.dbVersion;
+  const databaseMaxConnectionsValue = await health.dbMaxConnections;
+  const databaseOpenedConnectionsValue = await health.dbOpenedConnections
 
   response.status(200).json({
     updated_at: updatedAt,
