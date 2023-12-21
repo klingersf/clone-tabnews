04035d20e3194c4baf4e0c784a7516ddd8835dd7
Klinger Ferreira <60621086+klingersf@users.noreply.github.com>
2023-12-21 14:51:43 -0300

    Update dbase.js

--- infra/dbase.js
+++ infra/dbase.js
@@ -1,6 +1,7 @@
 import { Client } from "pg";
 
-  const client = new Client({
+async function connect() {
+    const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
@@ -8,18 +9,22 @@ import { Client } from "pg";
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
+  return client;
+}
 
 async function query(queryObject) {
+  const cnx = await connect()
   try {
     const result = queryObject;
     return result;
   } catch (err) {
     console.log(err);
   } finally {
-    await client.end();
+    await connect.end();
   }
 }
 
 export default {
+  conn: connect,
   query: query,
 };
